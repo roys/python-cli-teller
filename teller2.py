9f7c7df856cace1bafb9c16be5e491eaa63d095c
Roy Solberg <git@roysolberg.com>
2021-11-23 22:48:06 +0100

    Had to add gnureadline to make tab completion work on M1.

--- teller2.py
+++ teller2.py
@@ -16,6 +16,12 @@ import requests
 import sys
 import time
 import traceback
+import readline
+import rlcompleter
+if 'libedit' in readline.__doc__:
+    readline.parse_and_bind("bind ^I rl_complete")
+else:
+    readline.parse_and_bind("tab: complete")
 
 """
 TODO:
